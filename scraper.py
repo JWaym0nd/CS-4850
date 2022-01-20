af812277e3d2ef6905441a162d65ed23dec4d3b9
Jairrel <JWaym0nd@Yahoo.com>
2022-01-20 15:22:16 -0500

    Hardcoded the Marietta location

--- scraper.py
+++ scraper.py
@@ -1,10 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://www.doordash.com/food-delivery/marietta-ga-restaurants/")
-page
+# for Grubhub - hardcoded to Marietta, Ga
+page = requests.get("https://bit.ly/3tEGNJM")
+
+# for DoorDash - hardcoded to Marietta, Ga
+page1 = requests.get("https://www.doordash.com/food-delivery/marietta-ga-restaurants/")
+
+# placeholder
+page2 = ""
+
 print("Status code: " + str(page.status_code) + '\n')
 page.content
 
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+#print(soup.prettify())
