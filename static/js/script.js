24b3eed1677fd8df83cb245b4ca15de54ebc1968
Munir <munir@Munirs-MacBook-Pro.local>
2022-02-12 22:53:00 -0500

    scripts.js updated

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,21 @@
-ECHO is on.
+function showLocation()
+{
+	if(document.getElementById("location").style.display == "none")
+		document.getElementById("location").style.display = "block";
+	else
+		document.getElementById("location").style.display = "none";
+}
+function showRestaurant()
+{
+	if(document.getElementById("restaurant").style.display == "none")
+		document.getElementById("restaurant").style.display = "block";
+	else
+		document.getElementById("restaurant").style.display = "none";
+}
+function showOrder()
+{
+	if(document.getElementById("order").style.display == "none")
+		document.getElementById("order").style.display = "block";
+	else
+		document.getElementById("order").style.display = "none";
+}
