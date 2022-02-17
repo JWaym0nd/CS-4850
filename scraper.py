9e95ebed031c545c8d1010083618b7d8e495e301
Samuel Megginson <38111067+Biblioburro@users.noreply.github.com>
2022-02-17 11:32:50 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,10 +1,27 @@
-import requests
-from bs4 import BeautifulSoup
+import time
+import re
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 
-page = requests.get("https://www.doordash.com/food-delivery/marietta-ga-restaurants/")
-page
-print("Status code: " + str(page.status_code) + '\n')
-page.content
+driver = webdriver.Chrome(executable_path=r'C:\Users\johnr\PycharmProjects\pythonProject\chromedriver.exe')
+driver.get("https://www.grubhub.com/")
+address = "Marietta, GA 30067"
+driver.find_element(By.TAG_NAME, "input").send_keys(address)
+driver.find_element(By.TAG_NAME, "input").send_keys(Keys.ENTER)
+time.sleep(1)
+restaurant = "Burger King"
+driver.find_element(By.ID, "search-autocomplete-input").send_keys(restaurant)
+driver.find_element(By.ID, "search-autocomplete-input").send_keys(Keys.ENTER)
+time.sleep(1)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+ht = driver.find_elements(By.TAG_NAME, 'span')
+for elements in ht:
+    values = elements.text
+    break
+if values.count(restaurant) >= 3:
+    found = re.search('\\$(.+?) delivery', values).group()
+    print(found)
+else:
+    print("The restaurant you're looking for isn't on GrubHub at this time.")
+driver.close()
