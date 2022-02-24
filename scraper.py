7d53f4011cc061fddb54c8ee1e2cc1a61400b1e2
juanrios210 <98121609+juanrios210@users.noreply.github.com>
2022-02-24 14:50:39 -0500

    Added CSV File

--- scraper.py
+++ scraper.py
@@ -1,27 +1,45 @@
 import time
 import re
+import csv
+# import sys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path=r'C:\Users\johnr\PycharmProjects\pythonProject\chromedriver.exe')
-driver.get("https://www.grubhub.com/")
 address = "Marietta, GA 30067"
+restaurant = "Taco Bell"
+
+# try:
+# restaurant = sys.argv[1]
+# address = sys.argv[2]
+# except:
+# print("Unable to take cmd arguments.")
+
+driver = webdriver.Chrome(executable_path=r'C:\Users\Owner\PycharmProjects\pythonScraper\chromedriver.exe')
+driver.get("https://www.grubhub.com/")
+
 driver.find_element(By.TAG_NAME, "input").send_keys(address)
 driver.find_element(By.TAG_NAME, "input").send_keys(Keys.ENTER)
-time.sleep(1)
-restaurant = "Burger King"
+time.sleep(2)
+
 driver.find_element(By.ID, "search-autocomplete-input").send_keys(restaurant)
 driver.find_element(By.ID, "search-autocomplete-input").send_keys(Keys.ENTER)
-time.sleep(1)
+time.sleep(2)
 
 ht = driver.find_elements(By.TAG_NAME, 'span')
 for elements in ht:
     values = elements.text
     break
-if values.count(restaurant) >= 3:
+if values.count(restaurant) >= 0:
     found = re.search('\\$(.+?) delivery', values).group()
     print(found)
 else:
     print("The restaurant you're looking for isn't on GrubHub at this time.")
 driver.close()
+
+header = ['Address', 'Restaurant', 'Delivery Fee']
+data = [address, restaurant, found]
+with open('web-order.csv', 'w') as w:
+    writer = csv.writer(w)
+    writer.writerow(header)
+    writer.writerow(data)
