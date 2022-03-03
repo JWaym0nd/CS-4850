6670762618175a464cc54c7cc1895f95064854b2
MunirNaA <55366073+MunirNaA@users.noreply.github.com>
2022-03-03 14:49:09 -0500

    latitude and longitude updated

--- static/js/script.js
+++ static/js/script.js
@@ -19,3 +19,26 @@ function showOrder()
 	else
 		document.getElementById("order").style.display = "none";
 }
+var x = document.getElementById("latlong");
+function getLocation() {
+	var zipcode = document.getElementById('zipcode').value;
+	
+	if(zipcode == "")
+	{
+		document.getElementById("zipcode").focus();
+		document.getElementById("zipcodeerror").innerHTML = "Please Enter Your Zip Code";
+	}
+	else
+	{
+		document.getElementById("zipcodeerror").innerHTML = "";
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition);
+		  } else { 
+			x.innerHTML = "Geolocation is not supported by this browser.";
+		  }
+	}				  
+}
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
