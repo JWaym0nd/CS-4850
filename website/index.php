01a057548f4710f068ae6b30655dbf2e254595b0
tomshady13 <98121764+tomshady13@users.noreply.github.com>
2022-03-15 08:40:44 +0100

    Add files via upload

--- website/index.php
+++ website/index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
 <head>
     <meta charset="utf-8">
     <title>Smartchoice - The best food delivery option</title>
     <link rel="stylesheet" href="css/styles.css">
-	<link href="files/imgs/favicon.ico" rel="shortcut icon"/>
+    <link href="files/imgs/favicon.ico" rel="shortcut icon" />
     <!-- stylesheet Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
@@ -25,47 +26,61 @@
         <div class="searchForm">
             <form class="searchRestaurantForm" method="post">
                 <input type="text" name="zipcode" id="zipcode" class="search-btn" value="" placeholder="Enter your zip code">
-				<br/>
-				<input type="button" class="search" name="Search" id="Search" value="Search" onclick="getLocation()" />
-				<br/>
+                <input type="text" name=".." class=" search-btn" value="" placeholder="Enter ....">
+                <input type="text" name=".." class="search-btn" value="" placeholder="Enter ....">
+                <br/>
+                <input type="button" class="search" name="Search" id="Search" value="Search" onclick="getLocation()" />
+                <br/>
             </form>
-			<span id="zipcodeerror"></span>
-			<div id="latlong"></div>
+            <span id="zipcodeerror"></span>
+            <div id="latlong"></div>
         </div>
+
     </div>
 
 
     <div class="icon-bar">
 
-	<a href="https://www.doordash.com/"><img src="files/imgs/doordash.png" align="left" width="100" height="70"></a>
-<a href="https://www.grubhub.com/"><img src="files/imgs/grubhub.png" align="left" width="100" height="70"></a>
-		<a href="https://postmates.com/"><img src="files/imgs/postmates.jpg" align="left" width="100" height="70"></a>
+        <a href="https://www.doordash.com/"><img src="files/imgs/doordash.png" align="left" width="100" height="70"></a>
+        <a href="https://www.grubhub.com/"><img src="files/imgs/grubhub.png" align="left" width="100" height="70"></a>
+        <a href="https://postmates.com/"><img src="files/imgs/postmates.jpg" align="left" width="100" height="70"></a>
 
-	</div>
+    </div>
 
     <div id="feature-block">
         <div class="row feature-btn">
-            <div class="col-md-4" onclick="showLocation()">
-                <img class="img_location" src="files/imgs/location.png" alt="">
-                <h2>Your Location</h2>
-                <p>Select your location to find best prices</p>
-				<span id="location" style="display: none;">New York, NYC, USA</span>
+            <div class="text_decoration col-md-4" onclick="showLocation()">
+                <a href="location.html">
+                    <img class="img_location" src="files/imgs/location.png" alt="">
+                    <h2>Your Location</h2>
+                    <p>Select your location to find best prices</p>
+                    <span id="location" style="display: none;">New York, NYC, USA</span>
+                </a>
             </div>
-            <div class="col-md-4" onclick="showRestaurant()">
-                <img src="files/imgs/Storefront.png" alt="">
-                <h2>Restaurant</h2>
-                <p>Search and choose from our wide range of Restaurants</p>
-				<span id="restaurant" style="display: none;">Available Restaurants are Crown Shy, Atoboy</span>
+
+            <div class="text_decoration col-md-4" onclick="showRestaurant()">
+                <a href="rest.html">
+                    <img src="files/imgs/Storefront.png" alt="">
+                    <h2>Restaurant</h2>
+                    <p>Search and choose from our wide range of Restaurants</p>
+                    <span id="restaurant" style="display: none;">Available Restaurants are Crown Shy, Atoboy</span>
+                </a>
             </div>
-            <div class="col-md-4" onclick="showOrder()">
-                <img src="files/imgs/order.png" style="width: 154px; height: 154px;" alt="">
-                <h2>Order</h2>
-                <p>Make an order now!</p>
-				<span id="order" style="display: none;">Order Online for Home Delivery</span>
+
+
+            <div class="text_decoration col-md-4" onclick="showOrder()">
+                <a href="order.html">
+                    <img src="files/imgs/order.png" style="width: 154px; height: 154px;" alt="">
+                    <h2>Order</h2>
+                    <p>Make an order now!</p>
+                    <span id="order" style="display: none;">Order Online for Home Delivery</span>
+                </a>
             </div>
+
         </div>
     </div>
     </div>
-	<script src='javascript/scripts.js'></script>
+    <script src='javascript/scripts.js'></script>
 </body>
+
 </html>
