1d3ddd8dbfbcc3ca468f7ea3530f829c2fa3bf5d
juanrios210 <98121609+juanrios210@users.noreply.github.com>
2022-03-15 15:23:29 -0400

    Added Ubereats and Postmates
    All site functionality added, no item locators yet.

--- scraper.py
+++ scraper.py
@@ -7,7 +7,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 address = "Marietta, GA 30067"
-restaurant = "Taco Bell"
+restaurant = "Burger King"
+values = ""
 
 # try:
 # restaurant = sys.argv[1]
@@ -15,31 +16,79 @@ restaurant = "Taco Bell"
 # except:
 # print("Unable to take cmd arguments.")
 
-driver = webdriver.Chrome(executable_path=r'C:\Users\Owner\PycharmProjects\pythonScraper\chromedriver.exe')
-driver.get("https://www.grubhub.com/")
+driver = webdriver.Chrome(executable_path=r'C:\Users\johnr\PycharmProjects\pythonProject\chromedriver.exe')
 
+driver.get("https://www.grubhub.com/")
 driver.find_element(By.TAG_NAME, "input").send_keys(address)
+time.sleep(1)
 driver.find_element(By.TAG_NAME, "input").send_keys(Keys.ENTER)
 time.sleep(2)
 
 driver.find_element(By.ID, "search-autocomplete-input").send_keys(restaurant)
 driver.find_element(By.ID, "search-autocomplete-input").send_keys(Keys.ENTER)
-time.sleep(2)
+time.sleep(1)
 
-ht = driver.find_elements(By.TAG_NAME, 'span')
-for elements in ht:
+site1 = driver.find_elements(By.TAG_NAME, 'div')
+time.sleep(2)
+for elements in site1:
     values = elements.text
     break
 if values.count(restaurant) >= 0:
-    found = re.search('\\$(.+?) delivery', values).group()
-    print(found)
+    found1 = re.search('\\$(.+?) delivery', values).group(0)
+    print(found1)
 else:
     print("The restaurant you're looking for isn't on GrubHub at this time.")
+
+driver.get("https://www.ubereats.com/")
+driver.find_element(By.ID, "location-typeahead-home-input").send_keys(address)
+time.sleep(1)
+driver.find_element(By.ID, "location-typeahead-home-input").send_keys(Keys.ENTER)
+time.sleep(2)
+
+driver.find_element(By.ID, "search-suggestions-typeahead-input").send_keys(restaurant)
+driver.find_element(By.ID, "search-suggestions-typeahead-input").send_keys(Keys.ENTER)
+time.sleep(1)
+
+site2 = driver.find_elements(By.TAG_NAME, 'div')
+time.sleep(2)
+for elements in site2:
+    values = elements.text
+    break
+if values.count(restaurant) >= 0:
+    found2 = re.search('\\$(.+?) Delivery Fee', values).group(0)
+    print(found2)
+else:
+    print("The restaurant you're looking for isn't on UberEats at this time.")
+
+driver.get("https://www.postmates.com/")
+driver.find_element(By.TAG_NAME, "input").send_keys(address)
+time.sleep(2)
+driver.find_element(By.TAG_NAME, "input").send_keys(Keys.ENTER)
+time.sleep(2)
+
+driver.find_element(By.ID, "search-suggestions-typeahead-input").send_keys(restaurant)
+driver.find_element(By.ID, "search-suggestions-typeahead-input").send_keys(Keys.ENTER)
+time.sleep(1)
+
+site3 = driver.find_elements(By.TAG_NAME, 'div')
+time.sleep(2)
+for elements in site3:
+    values = elements.text
+    break
+if values.count(restaurant) >= 0:
+    found3 = re.search('\\$(.+?) Delivery Fee', values).group(0)
+    print(found3)
+else:
+    print("The restaurant you're looking for isn't on Postmates at this time.")
+
 driver.close()
 
 header = ['Address', 'Restaurant', 'Delivery Fee']
-data = [address, restaurant, found]
+data1 = [address, restaurant, found1]
+data2 = [address, restaurant, found2]
+data2 = [address, restaurant, found3]
 with open('web-order.csv', 'w') as w:
     writer = csv.writer(w)
     writer.writerow(header)
-    writer.writerow(data)
+    writer.writerow(data1)
+    writer.writerow(data2)
